9f28782941c0b6bfb3213af8704854ec12e75db8
qiaochao <qiaochao@icourt.cc>
2024-08-11 22:07:59 +0800

    feat: adjust compment address

--- image_gen/index.py
+++ image_gen/index.py
@@ -4,6 +4,28 @@ from pathlib import PurePosixPath
 import requests
 import dashscope
 
+# import replicate
+# replicate 的api_token, 但是他们要钱太厉害了
+# api_token = os.getenv('REPLICATE_API_TOKEN')
+# api_token = 'r8_M3Ua2fS3WN9fPwv5Lo24i7ALdFx7BOS0kv6sd'
+
+# # 初始化 Replicate 客户端
+# client = replicate.Client(api_token=api_token)
+
+
+# # 设置输入参数
+# input_params = {
+#     "prompt": "a photo of vibrant artistic graffiti on a wall saying \"SD3 medium\"",
+#     "aspect_ratio": "3:2",
+#     "image": "https://replicate.delivery/pbxt/LMbGi83qiV3QXR9fqDIzTl0P23ZWU560z1nVDtgl0paCcyYs/cars.jpg"
+# }
+
+# # 运行模型
+# output = client.run(
+#     "meta/sam-2:fe97b453a6455861e3bac769b441ca1f1086110da7466dbb65cf1eecfd60dc83",
+#     input=input_params
+# )
+
 
 model = "flux-schnell"
 prompt = "Eagle flying freely in the blue sky and white clouds"
